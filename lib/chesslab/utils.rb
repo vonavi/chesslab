ea8d1a1db49b71b4a407e63cd8291680e9570f5a
vonavi <ivvl82@gmail.com>
2014-08-29 18:13:12 +0400

    Symbolize the fractional part for Sonneborn-Berger score.

--- lib/chesslab/utils.rb
+++ lib/chesslab/utils.rb
@@ -5,20 +5,20 @@ module Chesslab
 
     # Replace the fractional part of number
     def symbolize_frac number
-      case number
-      when 0
-        return '0'
-      when 0.5
-        return '½'
-      end
+      return '' unless number
+      return '0' if number == 0
 
-      str  = Float(number).to_s
-      frac = str.split '.'
-      str  = case frac[1]
-      when '0'
-        frac[0]
-      when '5'
-        frac[0] + '½'
+      str       = Float(number).to_s
+      frac      = str.split '.'
+      frac_part = case frac[1]
+                  when '0'  then ''
+                  when '25' then '¼'
+                  when '5'  then '½'
+                  when '75' then '¾'
+                  end
+      if frac_part
+        int_part = (frac[0] == '0') ? '' : frac[0]
+        str = int_part + frac_part
       end
       str
     end
